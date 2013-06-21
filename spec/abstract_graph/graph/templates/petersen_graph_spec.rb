cd900562c1aeb2fbcfbe8ebdfe2d94782dbf7cf4
Austin Lee <Austin.L.D4L@gmail.com>
2013-06-21 12:56:06 -0700

    Add spec for petersen graphs. Part of #12

---
+++ spec/abstract_graph/graph/templates/petersen_graph_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module AbstractGraph
+  describe Graph do
+
+    describe ".petersen_graph" do
+
+      before :all do
+        @graphpetersen = Graph.petersen_graph
+      end
+
+      it "returns an object of class Graph" do
+        @graphpetersen.should be_an_instance_of(Graph)
+      end
+
+      it "contains 10 vertices and 15 edges" do
+        adjacencies = 0
+        (0..9).each do |i|
+          @graphpetersen.has_vertex("v#{2**i}").should be_true
+          adjacencies += @graphpetersen.adjacency_list("v#{2**i}").size
+        end
+        adjacencies.should eql(15 * 2)
+      end
+
+      it "ensures each vertex has degree 3" do
+        (0..9).each do |i|
+          @graphpetersen.adjacency_list("v#{2**i}").size.should eql(3)
+        end
+      end
+
+    end
+
+  end
+end
